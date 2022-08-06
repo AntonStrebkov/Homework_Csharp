a5b7f8ebb382289bc6ccfd8994d2877e4eaca031
AntonStrebkov <wancler987@gmail.com>
2022-08-06 19:51:55 +0700

    add Ex002 Max2

--- Ex001_Max/Program.cs
+++ Ex001_Max/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Введите первое целое число ");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт,
+//какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое целое число ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе целое чило ");
 int numberB = Convert.ToInt32(Console.ReadLine());
---
+++ Ex002_Max2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход три числа
+//и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое целое число ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье целое число ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+if (secondNumber > max)
+{
+    max = secondNumber;
+}
+if(thirdNumber > max)
+{
+    max = thirdNumber;
+}
+Console.WriteLine("Максимальное число: " + max);
