8c446b8c8f66ece68eb2a8477b54de4f69d594c1
AntonStrebkov <wancler987@gmail.com>
2022-08-06 19:30:26 +0700

    add Ex001 Max

---
+++ Ex001_Max/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое целое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое чило ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if(max < numberB)
+{
+    max = numberB;
+}
+Console.WriteLine("Большее число: " + max);
